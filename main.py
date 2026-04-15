fd920a4f33c4623d71c0b9f72db8b6a456dadbc3
TomasLopezDurand <tlopezdurand@udesa.edu.ar>
2026-04-15 12:17:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ Created on Wed Apr 15 12:07:15 2026
 @author: Tomas Lopez Durand
 """
 
+import cargar_datos
+import metricas
+import procesamiento_datos
+from cargar_datos import parsear_linea
+from cargar_datos import cargar_datos
+from metricas import calcular_tiempo_total
+from metricas import calcular_promedio_uso
+from metricas import calcular_uso_por_app
+from procesamiento_datos import filtrar_por_participante
+
