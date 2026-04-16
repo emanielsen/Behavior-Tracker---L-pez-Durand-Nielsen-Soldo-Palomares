9a524bea87f1915ec8532829f799a03123eac770
TomasLopezDurand <tlopezdurand@udesa.edu.ar>
2026-04-15 21:36:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ from procesamiento_datos import filtrar_por_participante
 archivo = "data/BehaviorTracker_mock_data"
 
 dicc_participantes = cargar_datos_a_dicc(archivo)
-promedio_uso = calcular_promedio_uso(dicc_participantes)
-uso_por_app = calcular_uso_por_app(dicc_participantes)
+
+while True:
+    try:
+        id_part = int(input("Ingrese el ID del participante al que se quiere buscar: "))
+    except ValueError:
+        
+
+participante = filtrar_por_participante(dicc_participantes, id_part)
+promedio_uso = calcular_promedio_uso(participante)
+uso_por_app = calcular_uso_por_app(participante)
 
