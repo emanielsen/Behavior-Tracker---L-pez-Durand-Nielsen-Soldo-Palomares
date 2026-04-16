d80ed68ef4d13b9ebb4d685576f4bfe472131bd7
TomasLopezDurand <tlopezdurand@udesa.edu.ar>
2026-04-15 21:12:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ Created on Wed Apr 15 12:07:15 2026
 import cargar_datos
 import metricas
 import procesamiento_datos
-from cargar_datos import parsear_linea
-from cargar_datos import cargar_datos
-from metricas import calcular_tiempo_total
+from cargar_datos import cargar_datos_a_dicc
 from metricas import calcular_promedio_uso
 from metricas import calcular_uso_por_app
 from procesamiento_datos import filtrar_por_participante
+
+archivo = "data/BehaviorTracker_mock_data"
+
+dicc_participantes = cargar_datos_a_dicc(archivo)
+promedio_uso = calcular_promedio_uso(dicc_participantes)
+uso_por_app = calcular_uso_por_app(dicc_participantes)
 
